5d83519cfd7502b1f45486ddf195c8861965fdaa
michaelstucki <orange.urn@gmail.com>
2020-07-29 17:22:15 -0600

    finis

--- cats_service.py
+++
@@ -1,6 +0,0 @@
-
-
-def get_cat(folder, name):
-    pass
-
-
--- program.py
+++ program.py
@@ -1,13 +1,14 @@
 import os
 import cat_service
+import subprocess
+import platform
 
 
 def main():
     print_header()
-    folder = do_output_folder()
-    # get/create output folder
-    # download cats
-    # display cats
+    folder = get_output_folder()
+    download_cats(folder)
+    display_cats(folder)
 
 
 def print_header():
@@ -16,7 +17,7 @@ def print_header():
     print('----------------------------')
 
 
-def do_output_folder():
+def get_output_folder():
     folder = 'cat_pictures'
     full_path = os.path.abspath(os.path.join('.', folder))
     if not os.path.exists(full_path) or not os.path.isdir(full_path):
@@ -27,10 +28,28 @@ def do_output_folder():
 
 
 def download_cats(folder):
-    cat_service.get_cat(folder, '')
+    print('Contacting server to download cats...')
+    cat_count = 8
+    for i in range(1, cat_count + 1):
+        name = "lolcat_{}".format(i)
+        print('Downloading cat ' + name)
+        cat_service.get_cat(folder, name)
+
+    print('Done.')
+
+
+def display_cats(folder):
+    # open folder
+    print('Displaying cats in OS window...')
+    if platform.system() == 'Darwin':
+        subprocess.call(['open', folder])
+    elif platform.system() == 'Windows':
+        subprocess.call(['exployer', folder])
+    elif platform.system() == 'Linux':
+        subprocess.call(['xgs-open', folder])
+    else:
+        print('We do not support your OS: {}.'.format(platform.system()))
 
 
 if __name__ == '__main__':
     main()
-
-
